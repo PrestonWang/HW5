01009869c452471bd8d58b1ba25c0e80c99229aa
PrestonWang <Preston@Prestons-MacBook-Pro.local>
2015-04-17 16:46:54 -0500

    Initializing hw5 repo

---
+++ HW5.X/HW5.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   HW5.c
+ * Author: Preston
+ *
+ * Created on April 17, 2015, 4:34 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
